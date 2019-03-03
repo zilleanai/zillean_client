ae7f904189afdeebc39537d29b078d7b5bf570bf
Christian M <chriamue@gmail.com>
2019-03-03 13:45:17 +0100

    added tests

--- tests/test_platform.py
+++ tests/test_platform.py
@@ -9,3 +9,18 @@ def test_testplatform():
     api = fileapi(project={'api_root': 'http://localhost:8888/api/v1/', 'name': 'test'})
     print(api.project['name'])
     print(api.list())
+
+def test_project():
+    from mlplatform_client.api import load
+    load('http://localhost:8888/api/v1/')
+    from  mlplatform_client.api.project import project as projectapi
+    api = projectapi(project={'api_root': 'http://localhost:8888/api/v1/', 'name': 'test'})
+    print(api.project['name'])
+    print(api.list())
+
+def test_tag():
+    from mlplatform_client.api import load
+    load('http://localhost:8888/api/v1/')
+    from  mlplatform_client.api.tag import tag as tagapi
+    api = tagapi(project={'api_root': 'http://localhost:8888/api/v1/', 'name': 'test'})
+    print(api.list())
