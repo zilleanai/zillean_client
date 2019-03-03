eed83a8a8b4247cfd321eb25ce440b0ab4af0017
Christian <chriamue@gmail.com>
2019-03-03 16:40:18 +0100

    refactored api

--- tests/test_platform.py
+++ tests/test_platform.py
@@ -1,26 +1,25 @@
 import pytest
-from mlplatform_client import Project
 
 
 def test_testplatform():
-    from mlplatform_client.api import load
+    from mlplatform_client import load
     load('http://localhost:8888/api/v1/')
-    from  mlplatform_client.api.file import file as fileapi
+    from  mlplatform_client.file import file as fileapi
     api = fileapi(project={'api_root': 'http://localhost:8888/api/v1/', 'name': 'test'})
     print(api.project['name'])
     print(api.list())
 
 def test_project():
-    from mlplatform_client.api import load
+    from mlplatform_client import load
     load('http://localhost:8888/api/v1/')
-    from  mlplatform_client.api.project import project as projectapi
+    from  mlplatform_client.project import project as projectapi
     api = projectapi(project={'api_root': 'http://localhost:8888/api/v1/', 'name': 'test'})
     print(api.project['name'])
     print(api.list())
 
 def test_tag():
-    from mlplatform_client.api import load
+    from mlplatform_client import load
     load('http://localhost:8888/api/v1/')
-    from  mlplatform_client.api.tag import tag as tagapi
+    from  mlplatform_client.tag import tag as tagapi
     api = tagapi(project={'api_root': 'http://localhost:8888/api/v1/', 'name': 'test'})
     print(api.list())
