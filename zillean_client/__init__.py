e2911295cde0f0f913713e63cd2024ae3140c964
Christian M <chriamue@gmail.com>
2019-04-11 10:36:52 +0200

    try catch added

--- zillean_client/__init__.py
+++ zillean_client/__init__.py
@@ -14,14 +14,18 @@ def load(host):
     modules = requests.get(url).json()['modules']
     print(__package__)
     for module in modules:
-        print('importing module:', module)
-        url = host + 'pyclient/' + module
-        response = requests.get(url)
-        fp = tempfile.NamedTemporaryFile()
-        fp.write(response.content)
-        fp.flush()
-        
-        modulename = __package__ + '.' + module
-        mod = importlib.machinery.SourceFileLoader(module, fp.name).load_module()
-        sys.modules[modulename] = mod
-        fp.close()
+        try:
+            print('importing module:', module)
+            url = host + 'pyclient/' + module
+            response = requests.get(url)
+            fp = tempfile.NamedTemporaryFile()
+            fp.write(response.content)
+            fp.flush()
+
+            modulename = __package__ + '.' + module
+            mod = importlib.machinery.SourceFileLoader(
+                module, fp.name).load_module()
+            sys.modules[modulename] = mod
+            fp.close()
+        except Exception as e:
+            print(e)
